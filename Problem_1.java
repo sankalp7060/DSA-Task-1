3c7876834f3b22557e47b3c03f1badc6cf9f7292
sankalp7060 <129958359+sankalp7060@users.noreply.github.com>
2024-10-21 10:48:28 +0530

    Add files via upload

---
+++ Problem_1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int c=0;
+        for(int i=0,j=0;i<g.length;){
+           if(j>=s.length){
+            break;
+           }
+           if(g[i]>s[j]){
+            j++;
+           }
+           else{
+            c++;
+            i++;
+            j++;
+           }
+        }
+        return c;
+    }
+}
